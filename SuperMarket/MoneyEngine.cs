8aef97f99e993dd251ecd437ef0e816a98cfb755
AnasChelli <37775415+AnasChelli@users.noreply.github.com>
2021-03-25 16:54:40 +0100

    Add project files.

---
+++ SuperMarket/MoneyEngine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperMarketTest
+{
+    public class MoneyEngine
+    {
+        private readonly List<long> _availablePieces = new List<long>
+        {
+            10,
+            5,
+            2
+        };
+
+        public Monnaie MonnaieOptimale(long moneyToBeReturned)
+        {
+
+            var filtredAvailablePieces = _availablePieces.Where(x => x <= moneyToBeReturned);
+            var optimalNumberOfPiecesByAvailablePieces = GetOptimal(moneyToBeReturned, filtredAvailablePieces);
+
+            return optimalNumberOfPiecesByAvailablePieces.Any() ? new Monnaie
+            {
+                Piece2 = optimalNumberOfPiecesByAvailablePieces.ContainsKey(2) ? optimalNumberOfPiecesByAvailablePieces[2] : 0,
+                Billet5 = optimalNumberOfPiecesByAvailablePieces.ContainsKey(5) ? optimalNumberOfPiecesByAvailablePieces[5] : 0,
+                Billet10 = optimalNumberOfPiecesByAvailablePieces.ContainsKey(10) ? optimalNumberOfPiecesByAvailablePieces[10] : 0
+            } : null;
+        }
+
+        private static IDictionary<long, long> GetOptimal(long moneyToBeReturned, IEnumerable<long> availablePieces)
+        {
+            long nbPieces = -1;
+            var optimalNumberOfPiecesByAvailablePieces = new Dictionary<long, long>();
+
+            foreach (var availablePiece in availablePieces)
+            {
+                while (nbPieces != 0)
+                {
+                    nbPieces = moneyToBeReturned % availablePiece;
+                    if (nbPieces == 1)
+                    {
+                        break;
+                    }
+
+                    optimalNumberOfPiecesByAvailablePieces.Add(availablePiece, moneyToBeReturned / availablePiece);
+                    moneyToBeReturned = nbPieces;
+                    break;
+                }
+            }
+
+            return optimalNumberOfPiecesByAvailablePieces;
+        }
+    }
+}
---
+++ SuperMarket/Monnaie.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperMarketTest
+{
+    public class Monnaie
+    {
+        public long Piece2 { get; set; }
+        public long Billet5 { get; set; }
+        public long Billet10 { get; set; }
+    }
+}
